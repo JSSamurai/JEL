60c4ff8d35928fbef83e14a28a319959cfc049ec
Katana-Development <katana.development@gmail.com>
2021-02-10 11:18:18 -0800

    Blue BG, Red Syntax, Green accent/trim

---
+++ assets/constants.js
@@ -0,0 +1,8 @@
+const ERROR_MSG = {
+    ERR_01: `\n\t A Type-Error has occured. An invalid type was passed into an instance of Class Log's constructor parameter, 'filepath'. Parameter 'filepath' must be of type string. Parameter 'filepath' must be a valid path to an existing file, or a valid path to an existing location where a file can be created.\n`,
+
+    ERR_02: `Empty`,
+
+    ERR_03: `Empty`,
+    ERR_04: `Empty`,
+};
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const { O_RDWR, O_CREAT } = require('constants');
+const fs = require('fs');
+
 class Log {
-  constructor(filepath) {
-    this.fpath = filepath;
-  }
+    #filepath = '/filepath';
+    #contents = '';
+
+    constructor(filepath) {
+        if (typeof filepath !== 'string') {
+            throw new TypeError('\n>> A type-error has occured...');
+        }
+
+        this.#filepath = filepath;
+
+        this.regularExpression = /^[a-z|0-9]{4,20}(.)*\d\w$/gim;
+
+        const fd = fs.openSync(filepath, O_RDWR | O_CREAT);
+        const str = fs.readSync(fd);
+    }
+
+    printLog() {
+        console.log();
+
+        fs.readSync();
+
+        return 124500;
+    }
 }
 
-class JelError {
-  constructor() {}
-}
+module.exports.class = Log;
